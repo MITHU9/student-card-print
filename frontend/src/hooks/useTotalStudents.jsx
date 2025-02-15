758148275a429ef1e2920d3e39e16a67f089e66b
Shahariar Mithu <kmmithu2015@gmail.com>
2025-02-15 15:11:43 +0600

    fixed some issue

--- frontend/src/hooks/useTotalStudents.jsx
+++ frontend/src/hooks/useTotalStudents.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
 
-const local = "http://localhost:5000";
+//const local = "http://localhost:5000";
 const remote = "https://library-card-backend.vercel.app";
 
 function useTotalStudents() {
@@ -10,7 +10,7 @@ function useTotalStudents() {
   const { data, refetch } = useQuery({
     queryKey: ["total-students"],
     queryFn: async () => {
-      const res = await axiosSecure.get(`${local}/total-students`);
+      const res = await axiosSecure.get(`${remote}/total-students`);
       return res.data;
     },
   });

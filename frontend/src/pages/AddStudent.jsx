985bd921f88f672ee4339ebe8a780908adb879a8
Shahariar Mithu <kmmithu2015@gmail.com>
2025-03-15 23:25:39 +0600

    add sample csv file

--- frontend/src/pages/AddStudent.jsx
+++ frontend/src/pages/AddStudent.jsx
@@ -62,21 +62,46 @@ const AddStudent = () => {
         </nav>
       </header>
       <div className=" bg-white rounded-lg shadow-md mt-10 p-6">
-        <h2 className="text-xl font-bold mb-4">
-          Upload Student data (CSV file)
-        </h2>
-        <input
-          type="file"
-          accept=".csv"
-          onChange={handleFileChange}
-          className="mb-4 border border-gray-300 p-2 rounded-md mr-2 cursor-pointer"
-        />
-        <button
-          onClick={handleUpload}
-          className="bg-blue-500 text-white px-4 py-2 rounded-lg cursor-pointer hover:bg-blue-600"
-        >
-          {loading ? "Uploading..." : "Upload"}
-        </button>
+        <div className="flex justify-between items-center">
+          <div>
+            <h2 className="text-xl font-bold mb-4">
+              Upload Student data (.CSV file)
+            </h2>
+            <input
+              type="file"
+              accept=".csv"
+              onChange={handleFileChange}
+              className="mb-4 border border-gray-300 p-2 rounded-md mr-2 cursor-pointer"
+            />
+            <button
+              onClick={handleUpload}
+              className="bg-blue-500 text-white px-4 py-2 rounded-lg cursor-pointer hover:bg-blue-600"
+            >
+              {loading ? "Uploading..." : "Upload"}
+            </button>
+          </div>
+
+          <div className=" flex flex-col p-4 items-end">
+            <p className="w-1/2 mt-4 text-sm font-semibold text-red-600">
+              <span className="text-gray-700 font-bold text-xl">Note:</span> In
+              order to upload student data, you need to have a{" "}
+              <strong className="italic">CSV file</strong> with the following
+              {""}
+              <strong> columns name</strong> in the first row with{" "}
+              <strong>case sensitive</strong> in this file.
+            </p>
+            <div className="flex items-center gap-2">
+              <h3 className="font-bold">Download sample CSV file:</h3>
+              <a
+                href="/demo.csv"
+                className="bg-blue-500 text-white px-4 py-2 rounded-lg cursor-pointer hover:bg-blue-600"
+                download
+              >
+                Download
+              </a>
+            </div>
+          </div>
+        </div>
 
         {data.length > 0 && (
           <div className="mt-4">
